0de1268250322d8f729b0086eb10778007e3e257
Jose <Jose@192.168.0.7>
2024-06-01 01:08:40 -0300

    Comentarios

--- MiRobot/src/ar/edu/et7/MiRobot.java
+++ MiRobot/src/ar/edu/et7/MiRobot.java
@@ -13,12 +13,16 @@ public class MiRobot extends Robot {
 
 		while (true) {
 
+			//Desplaza el robot 100 pixels hacia adelante
 			ahead(100);
 
+			//Gira el cañon hacia la derecha 360 grados
 			turnGunRight(360);
 
+			//Desplaza el robot 100 pixels hacia atras
 			back(100);
 
+			//Gira el cañon hacia la izquierda 360 grados
 			turnGunRight(360);
 		}
 	}
