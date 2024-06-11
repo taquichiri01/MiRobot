3310d475c6d0edb5a0163a1b2559f86b1630282e
ET7 <ET7@DESKTOP-1FNGV7Q>
2024-06-11 15:37:45 -0300

    cambios: desplazamiento del robot a 10 pixels para adelante.  aumento de "fire" a 100000000.

---
+++ MiRobot/src/ar/edu/et7/RobotDeSanti.java
@@ -0,0 +1,73 @@
+package ar.edu.et7;
+
+import robocode.Robot;
+import robocode.Rules;
+import robocode.ScannedRobotEvent;
+import robocode.HitWallEvent;
+import robocode.BulletHitEvent;
+
+/**
+ * @author Maximo Kaled De Santi
+ * 
+ */
+
+public class RobotDeSanti extends Robot {
+
+	public void run() {
+
+		while (true) {
+
+			// Desplaza el robot 100 pixels hacia adelante
+			ahead(100);
+
+			// Gira el cañon hacia la derecha 90 grados
+			turnGunRight(360);
+
+			// Dispara con la maxima potencia
+			fire(Rules.MAX_BULLET_POWER);
+
+			// Desplaza el robot 100 pixels hacia adelante
+			ahead(100);
+
+			// Gira el cañon hacia la derecha 180 grados
+			turnGunLeft(90);
+
+			// Dispara con la maxima potencia
+			fire(Rules.MAX_BULLET_POWER);
+
+			// Gira hacia la izquierda 90 grados
+			turnLeft(90);
+
+			// Gira el radar hacia la izquierda 15 grados
+			turnRadarLeft(15);
+			
+			// Escanear otros robots
+			scan();
+			
+		}
+	}
+
+	public void onScannedRobot(ScannedRobotEvent event) {
+
+		// Al escanear un robot
+		if (event.getDistance() < 100) {
+			// Disparar
+			fire(100000000);
+		} else {
+			// Disparar
+			fire(5000000);
+		}
+	}
+	
+	   public void onHitWall(HitWallEvent event) {
+
+		   // Al colisionar contra una pared
+	       out.println("Choque contra la pared en direccion " + event.getBearing() + " degrees.");
+	   }	
+	   
+	   public void onBulletHit(BulletHitEvent event) {
+		   
+		   // Al acertar un disparo
+	       out.println("Le acerte un disparo a " + event.getName() + "!");
+	   }	   
+}
