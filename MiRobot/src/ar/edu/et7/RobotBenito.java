6c7e085c463eb620717b220562c3625e309f3708
ET7 <yamil.taquichiri.et7@gmail.com>
2024-06-11 16:27:18 -0300

    hola

---
+++ MiRobot/src/ar/edu/et7/RobotBenito.java
@@ -0,0 +1,80 @@
+package ar.edu.et7;
+
+import robocode.Robot;
+import robocode.Rules;
+import robocode.ScannedRobotEvent;
+import robocode.HitWallEvent;
+import robocode.BulletHitEvent;
+
+/**
+ * @author Alan Pinto
+ * 
+ */
+
+public class RobotBenito extends Robot {
+
+	public void run() {
+
+		while (true) {
+
+			// Desplaza el robot 100 pixels hacia adelante
+						ahead(100);
+
+						// Gira el cañon hacia la derecha 90 grados
+						turnGunRight(140);
+
+						// Dispara con la maxima potencias
+						fire(Rules.MAX_BULLET_POWER);
+
+						// Desplaza el robot 100 pixels hacia adelantes
+						ahead(100);
+						
+						fire(Rules.MAX_BULLET_POWER);
+
+						// Gira el cañon hacia la derecha 180 grados
+						turnGunLeft(1000);
+
+						// Dispara con la maxima potencia
+						fire(Rules.MAX_BULLET_POWER);
+
+						// Gira hacia la izquierda 90 grados
+						turnLeft(120);
+						
+						fire(Rules.MAX_BULLET_POWER);
+						// Gira el radar hacia la izquierda 15 grados
+						turnRadarLeft(90);
+						
+						turnGunRight(140);
+						
+						fire(Rules.MAX_BULLET_POWER);
+						
+						// Escanear otros robots
+						scan(); 
+			
+		}
+	}
+
+	public void onScannedRobot(ScannedRobotEvent event) {
+
+		// Al escanear un robot
+		if (event.getDistance() < 100) {
+			// Disparar
+			fire(10);
+		} else {
+			// Disparar
+			fire(5);
+		}
+	}
+	
+	   public void onHitWall(HitWallEvent event) {
+
+		   // Al colisionar contra una pared
+	       out.println("Choque contra la pared en direccion " + event.getBearing() + " degrees.");
+	   }	
+	   
+	   public void onBulletHit(BulletHitEvent event) {
+		   
+		   // Al acertar un disparo
+	       out.println("Le acerte un disparo a " + event.getName() + "!");
+	   }	   
+}
